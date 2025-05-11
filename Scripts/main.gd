dd060729d9c5bdec75fba5fccc84494cd54bc2d3
emilyallerdings <skittlesanator@gmail.com>
2025-05-11 01:39:43 -0600

    Revert "merge"
    This reverts commit e016e7cdc76f04a7fef5ff8800346ea698c663a5.

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,24 +3,14 @@ extends Node3D
 @onready var player: CharacterBody3D = $Player
 @onready var main_camera: Camera3D = $MainCamera
 
-var playing = false
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-<<<<<<< Updated upstream
 	SoundBus.song_1.play()
-=======
-	playing = true
-	pass # Replace with function body.
->>>>>>> Stashed changes
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	main_camera.global_position.z = $Player.global_position.z - 2.5
-	if playing && $Player.global_position.z >= $BoardingRamp.length - 5.0:
-		print("you win!")
-		playing = false
 	pass
 
 # YAY
