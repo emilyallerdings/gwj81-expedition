819346c5b186920a7caf8a4aadcba225ff7700ac
Andrew Kim <ndbkim@gmail.com>
2025-05-10 23:14:28 -0700

    Selection menu

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,11 +3,15 @@ extends Node3D
 @onready var suitecase_sprite = $SuitecaseSprite
 
 #region menus
+@onready var main_menu_camera = $"Main Menu/Main Menu Camera"
 @onready var main_menu_ui = $"Main Menu/Main Menu UI"
+
+@onready var options_menu_camera = $"Options Menu/Options Menu Camera"
 @onready var options_menu_ui = $"Options Menu/Options Menu UI"
 
-@onready var main_menu_camera = $"Main Menu/Main Menu Camera"
-@onready var options_menu_camera = $"Options Menu/Options Menu Camera"
+@onready var selection_menu_camera = $"Selection Menu/Selection Menu Camera"
+@onready var selection_menu_ui = $"Selection Menu/Selection Menu UI"
+
 #endregion
 
 #region main menu ui buttons
@@ -18,6 +22,10 @@ extends Node3D
 
 #region options menu ui buttons
 @onready var options_back = $"Options Menu/Options Menu UI/Options Back"
+#endregion
+
+#region selection menu ui buttons
+
 #endregion
 
 var camera_transition := false
@@ -41,11 +49,11 @@ func lerp_camera(delta: float) -> void:
 		to_camera.make_current()
 		from_camera.clear_current()
 		
+		#TODO Tween the behavior
 		# no idea how to make the camera switch smooth
 		#from_camera.position.slerp(to_camera.position, transition_speed)
 		#from_camera.rotation.slerp(to_camera.rotation, transition_speed)
 		camera_transition = false
-	
 
 func transition_cameras(curr_camera: Camera3D, next_camera: Camera3D) -> void:
 	from_camera = curr_camera
@@ -55,6 +63,12 @@ func transition_cameras(curr_camera: Camera3D, next_camera: Camera3D) -> void:
 	
 func _on_play_pressed() -> void:
 	SoundBus.button.play()
+	SoundBus.whoosh.play()
+	transition_cameras(main_menu_camera, selection_menu_camera)
+	
+	selection_menu_ui.visible = true
+	await get_tree().create_timer(0.5)
+	main_menu_ui.visible = false
 
 func _on_options_pressed() -> void:
 	SoundBus.button.play()
@@ -91,6 +105,6 @@ func _on_credits_mouse_entered() -> void:
 	suitecase_sprite.position = Vector2(credits.position.x - 50.0, credits.position.y + 35.0)
 	SoundBus.button_hover_click.play()
 
-func _on_options_back_mouse_entered():
+func _on_options_back_mouse_entered() -> void:
 	SoundBus.button_hover_click.play()
 	suitecase_sprite.position = Vector2(options_back.position.x - 50.0, options_back.position.y + 35.0)
