18c61ce4882549234f5979685e71028d28f970c2
Andrew Kim <ndbkim@gmail.com>
2025-05-10 18:36:21 -0700

    Added suitcase and fun little mouse thing

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+@onready var play = $"Main Menu UI/Play"
+@onready var options = $"Main Menu UI/Options"
+@onready var credits = $"Main Menu UI/Credits"
+@onready var suitecase_sprite = $SuitecaseSprite
 
 func _ready():
 	SoundBus.airport_ambience.play()
@@ -13,3 +17,12 @@ func _on_options_pressed():
 
 func _on_credits_pressed():
 	SoundBus.button.play()
+
+func _on_play_mouse_entered():
+	suitecase_sprite.position = play.position + Vector2(-5.0, play.position.y)
+
+func _on_options_mouse_entered():
+	suitecase_sprite.position = options.position + Vector2(-5.0, options.position.y)
+
+func _on_credits_mouse_entered():
+	suitecase_sprite.position = credits.position + Vector2(-5.0, credits.position.y)
