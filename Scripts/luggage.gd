a2b154ccf99d52542b011e1a1de26ed7e5b8223f
Andrew Kim <ndbkim@gmail.com>
2025-05-15 02:15:39 -0700

    updated luggage stats for more tuning

--- Scripts/luggage.gd
+++ Scripts/luggage.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
 @export var title : String = "" 
-@export var top_speed := 5.0
-@export var handling := 5.0
-@export var boost := 5.0
+@export var top_speed := 10.0
+@export var handling := 16.0
+@export var boost := 10.0
 @export_multiline var description : String = ""
 
 @export var font : FontFile = preload("res://Materials/NixieOne.ttf")
