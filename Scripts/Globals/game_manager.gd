597e92520eebfaa72b18a3e40cf91c8324b31a68
emilyallerdings <skittlesanator@gmail.com>
2025-05-18 07:04:02 -0600

    dsds

--- Scripts/Globals/game_manager.gd
+++ Scripts/Globals/game_manager.gd
@@ -9,7 +9,7 @@ var difficulty = 5.0 # for testing my branch
 var chosen_luggage : PackedScene = null
 var base_difficulty : int = 0
 var modifier_difficulty : int = 0
-var current_level : int = 0
+var current_level : int = 9
 
 #In CENTS
 var total_money : int = 0
